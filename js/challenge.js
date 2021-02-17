551ece468ba78e4f8998bd0ad4e7edc03972f10d
Amanda Rolon <abrolon87@gmail.com>
2021-02-16 20:55:15 -0500

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -1,5 +1,14 @@
-counter = document.getElementById("counter").addEventListener("click", startCount)
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+const counter = document.getElementById("counter");
+const pause = document.getElementById("pause");
+let count = counter.innerText
 
-function startCount(e) {
-  
-}
+// document.addEventListener("DOMContentLoaded", (e) => {
+
+// })
+
+plus.addEventListener("click", () => {
+  counter.innerText = parseInt(counter.innerText) + 1
+})
+
